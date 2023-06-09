f4367484f91e19e0471b1c1f5e21847aa9d276b7
the-albertox <usr3562@salesianos-lacuesta.net>
2023-06-09 12:00:01 +0100

    feat: mod Player class to add attack & RecibirDanio

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,6 +1,8 @@
 package player;
 
-public class Player {
+import interfaces.Combate;
+
+public class Player implements Combate {
     protected int health;
     protected int defense;
     protected int strength;
@@ -10,5 +12,40 @@ public class Player {
         this.defense = defense;
         this.strength = strength;
     }
- 
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    
+    @Override
+    public int attack(){
+        return strength ;
+    }
+
+    @Override
+    public void recibirDanio(int attack){
+        int Danio = attack - defense;
+        health -= Danio;
+        System.out.println("your character has received" + Danio + " points of damage, its health now is " + health );
+    }
 }
